1ab14e92c41501a5c46d838c745263a71ed1425d
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-09 22:03:57 +0300

    began planning the spherical_harmonics.c file

--- spherical_harmonics/c/spherical_harmonics.c
+++ spherical_harmonics/c/spherical_harmonics.c
@@ -1,1 +1,144 @@
 // TODO
+
+void r_init_shc()
+{
+    // TODO: initialize struct containing the bandlimit and coefficients
+    // coefficients are given as pointer to array of appropriate sizeZ
+}
+
+void c_init_shc()
+{
+    // TODO: initialize struct containing the bandlimit and coefficients
+    // coefficients are given as pointer to array of appropriate sizeZ
+}
+
+void r_init_shc2()
+{
+    // TODO: initialize struct containing the bandlimit and coefficients
+    // space for coefficients is allocated and they are initialized to all zeros
+}
+
+void c_init_shc2()
+{
+    // TODO: initialize struct containing the bandlimit and coefficients
+    // space for coefficients is allocated and they are initialized to all zeros
+}
+
+void r_destroy_shc()
+{
+    // TODO: destroys struct containing the bandlimit and coefficients
+    // doesn't deallocate coefficients memory
+}
+
+void c_destroy_shc()
+{
+    // TODO: initialize struct containing the bandlimit and coefficients
+    // doesn't deallocate coefficients memory
+}
+
+void r_destroy_shc2()
+{
+    // TODO: initialize struct containing the bandlimit and coefficients
+    // deallocates coefficients memory
+}
+
+void c_destroy_shc2()
+{
+    // TODO: initialize struct containing the bandlimit and coefficients
+    // deallocates coefficients memory
+}
+
+double r_get_shc()
+{
+    // TODO
+}
+
+double c_get_shc()
+{
+    // TODO
+}
+
+void r_set_shc()
+{
+    // TODO
+}
+
+void c_set_shc()
+{
+    // TODO
+}
+
+void r_normalize_shc()
+{
+    // TODO
+}
+
+void c_normalize_shc()
+{
+    // TODO
+}
+
+void r_random_normalized_shc()
+{
+    // TODO
+}
+
+void c_random_normalized_shc()
+{
+    // TODO
+}
+
+double *r_allocate_shc()
+{
+    // TODO
+    // Allocate memory for SHC
+}
+
+double *c_allocate_shc()
+{
+    // TODO
+    // Allocate memory for SHC
+}
+
+double *r_deallocate_shc()
+{
+    // TODO
+}
+
+double *c_deallocate_shc()
+{
+    // TODO
+}
+
+void r_print_shc()
+{
+    // TODO
+}
+
+void c_print_shc()
+{
+    // TODO
+}
+
+void r_add_shc()
+{
+    // TODO: calcualte shc1+alpha*shc2
+    // Should use restricted pointers
+}
+
+void c_add_shc()
+{
+    // TODO: calcualte shc1+alpha*shc2
+    // Should use restricted pointers
+}
+
+void r_multiply_shc_in_place()
+{
+    // TODO: calculate alpha*shc2
+}
+
+void c_multiply_shc_in_place()
+{
+    // TODO: calculate alpha*shc2
+}
+
