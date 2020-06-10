e806d37b13ba39a4e34c79090ab35fc34f6dffdb
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-10 16:22:16 +0300

    wrote the .h file and fixed the includes in the .c file

--- clebsch_gordan/c/clebsch_gordan_coefficients.c
+++ clebsch_gordan/c/clebsch_gordan_coefficients.c
@@ -2,6 +2,7 @@
 
 
 #include <stdio.h>
+#include <math.h>
 #include "clebsch_gordan_coefficients.h"
 
 
--- clebsch_gordan/c/clebsch_gordan_coefficients.h
+++ clebsch_gordan/c/clebsch_gordan_coefficients.h
@@ -1,10 +1,8 @@
 // TODO: make sure it's correct
 
 #include <stdint.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdlib.h>
 
+/* Typedef */
 typedef double***** clebsch_gordan_table;
 typedef 
     struct cg_table
@@ -13,42 +11,37 @@ typedef
         size_t bandlimit;
     }  cg_table;
 
-long int clebsch_gordan_lower_bound(const long int l1, const long int l2, const long int m);
 
-long int clebsch_gordan_upper_bound(const long int l1, const long int l2, const long int m);
 
-void calculate_clebsch_gordan(const long int l1, const long int l2, const long int l, const long int m, double *cb);
+/* Function declarations */
+long cg_lower_bound(const long l1, const long l2, const long m);
 
-void calculate_clebsch_gordan_coefficients(const size_t bandlimit, cg_table clebsch_gordan_table);
 
-cg_table allocate_memeory_for_clebsch_gordan_coefficients(const size_t bandlimit);
+long cg_upper_bound(const long l1, const long l2, const long m);
 
-void free_memory_for_clebsch_gordan_coefficients(const size_t bandlimit, cg_table clebsch_gordan_table);
 
-bool validate_clebsch_gordan_table_order(   const size_t    bandlimit, 
-                                            const long int  l1, 
-                                            const long int  l2, 
-                                            const long int  l, 
-                                            const long int  m, 
-                                            const long int  m1);
+double backward_substitution_central_diagonal(const long l1, const long l2, const long l, const long m1, const long m);
 
-double get_clebsch_gordan_coefficient(  const cg_table  clebsch_gordan_table, 
-                                        const long int  l1, 
-                                        const long int  l2, 
-                                        const long int  l, 
-                                        const long int  m, 
-                                        const long int  m1);
 
-void set_clebsch_gordan_coefficient(    cg_table        clebsch_gordan_table, 
-                                        const long int  l1, 
-                                        const long int  l2, 
-                                        const long int  l, 
-                                        const long int  m, 
-                                        const long int  m1, 
-                                        const double  coefficient);
+double backward_substitution_secondary_diagonal(const long l1, const long l2, const long m1, const long m);
 
-long int maximum(long int a, long int b);
 
-long int minimum(long int a, long int b);
+void cg_vector(const long l1, const long l2, const long l, const long m, double * restrict cg);
 
-long int absolute_value(long int a);
+
+void calculate_cg_table(const size_t bandlimit, cg_table *table);
+
+
+cg_table allocate_cg_table(const size_t bandlimit);
+
+
+void destroy_cg_table(cg_table *table);
+
+
+double get_cg(const cg_table *table, const long l1, const long l2, const long l, const long m, const long m1);
+
+
+void set_cg(cg_table *table, const long l1, const long l2, const long l, const long m, const long m1, const double coefficient);
+
+
+void print_cg(cg_table *table);
