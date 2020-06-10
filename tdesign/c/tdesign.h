5ad637ded8655dd26d9264aa0125dc9960865c9e
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-10 21:00:36 +0300

    started writing the t-design code

--- tdesign/c/tdesign.h
+++ tdesign/c/tdesign.h
@@ -1,4 +1,23 @@
-// TODO
 
-// TODO: 
-const char **bandlimit_filename;
+const char *bandlimit_filename[];
+const long tdesign_length[];
+
+/* t-design in Cartesian coordinates. */
+typedef 
+    struct tdesign_cart
+    {
+        size_t bandlimit;
+        double *tdesign;
+    }
+    tdesign_cart;
+
+/* t-design in spherical coordinates. */
+typedef 
+    struct tdesign_sph
+    {
+        size_t bandlimit;
+        double *tdesign;
+    }
+    tdesign_sph;
+
+typedef enum COORD_SYSTEM {EUC, SPH} COORD_SYSTEM;
