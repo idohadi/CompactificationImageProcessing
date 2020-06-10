a51fa69bd0eaba8ca4f158dc42bbc58105c85794
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-10 14:32:35 +0300

    changed the names of functions and got rid of the use of minimum and absoluate_value functions

--- clebsch_gordan/c/clebsch_gordan_coefficients.c
+++ clebsch_gordan/c/clebsch_gordan_coefficients.c
@@ -36,8 +36,8 @@ double backward_substitution_secondary_diagonal(const long l1, const long l2, co
 void calculate_clebsch_gordan(const long l1, const long l2, const long l, const long m, double * restrict cg)
 {
     /*  I assume cg is an array of size (upper_bound - lower_bound + 1). */
-    long lower_bound = clebsch_gordan_lower_bound(l1, l2, m);
-    long upper_bound = clebsch_gordan_upper_bound(l1, l2, m);
+    long lower_bound = cg_lower_bound(l1, l2, m);
+    long upper_bound = cg_upper_bound(l1, l2, m);
 
     cg[upper_bound - lower_bound] = 1.0/(upper_bound - lower_bound + 1);
 
@@ -47,7 +47,8 @@ void calculate_clebsch_gordan(const long l1, const long l2, const long l, const 
 
         // Using the recursion to calculate the Clebsch-Gordan coefficients
         cg[upper_bound - lower_bound - 1] 
-            = -cg[upper_bound - lower_bound]*backward_substitution_central_diagonal(l1, l2, l, upper_bound, m)/backward_substitution_secondary_diagonal(l1, l2, upper_bound-1, m);
+            = -cg[upper_bound - lower_bound]*backward_substitution_central_diagonal(l1, l2, l, upper_bound, m)
+                /backward_substitution_secondary_diagonal(l1, l2, upper_bound-1, m);
         norm += cg[upper_bound - lower_bound - 1]*cg[upper_bound - lower_bound - 1];
 
         for (long i = upper_bound - lower_bound - 2; i>=0; --i)
@@ -70,18 +71,18 @@ void calculate_clebsch_gordan(const long l1, const long l2, const long l, const 
 
 void calculate_clebsch_gordan_coefficients(const size_t bandlimit, cg_table clebsch_gordan_table)
 {
-    /* Documentation of memory structure is in allocate_memeory_for_clebsch_gordan_coefficients */
+    /* Documentation of memory structure is in allocate_cg_table */
 
     #pragma omp parallel for collapse(2)
     for (long l1 = 0; l1<=bandlimit; ++l1)
     {
         for (long l2 = 0; l2<=bandlimit; ++l2)
         {
-            for (long l = absolute_value(l1-l2); l<=minimum(l1+l2, bandlimit); ++l)
+            for (long l = (l1>=l2 ? l1-l2 : l2-l1); l<=l1+l2 && l<=bandlimit; ++l)
             {
                 for (long m = -l; m<=l; ++m)
                 {
-                    calculate_clebsch_gordan(l1, l2, l, m, clebsch_gordan_table[l1][l2][l-absolute_value(l1-l2)][m+l]);
+                    calculate_clebsch_gordan(l1, l2, l, m, clebsch_gordan_table[l1][l2][l-(l1>=l2 ? l1-l2 : l2-l1)][m+l]);
                 }
             }
         }
@@ -89,7 +90,7 @@ void calculate_clebsch_gordan_coefficients(const size_t bandlimit, cg_table cleb
 }
 
 
-cg_table allocate_memeory_for_clebsch_gordan_coefficients(const size_t bandlimit)
+cg_table allocate_cg_table(const size_t bandlimit)
 {
     /* 
         Returns an array CGs with dimensions such that it is possible to assign:
@@ -103,105 +104,84 @@ cg_table allocate_memeory_for_clebsch_gordan_coefficients(const size_t bandlimit
                 min_m1:=max(-l1, m-l2)
                 max_m1:=min(l1,  m+l2)  */
 
-    cg_table clebsch_gordan_table = malloc((bandlimit+1)*sizeof(double ****));
+    cg_table table = malloc((bandlimit+1)*sizeof(double ****));
+
+    long min_val; 
+    long abs_val;
 
     for (long l1 = 0; l1<=bandlimit; ++l1)
     {
-        clebsch_gordan_table[l1] = malloc((bandlimit+1)*sizeof(double ***));
+        table[l1] = malloc((bandlimit+1)*sizeof(double ***));
         
         for (long l2 = 0; l2<=bandlimit; ++l2)
         {
-            clebsch_gordan_table[l1][l2] = malloc((minimum(l1+l2, bandlimit) - absolute_value(l1-l2) + 1)*sizeof(double **));
+            min_val = l1+l2>=bandlimit ? bandlimit : l1+l2;
+            abs_val = l1>=l2 ? l1-l2 : l2-l1;
 
-            for (long l = absolute_value(l1-l2); l<=minimum(l1+l2, bandlimit); ++l)
+            table[l1][l2] = malloc((min_val - abs_val + 1)*sizeof(double **));
+
+            for (long l = abs_val; l<=min_val; ++l)
             {
-                clebsch_gordan_table[l1][l2][l- absolute_value(l1-l2)] = malloc((2*l+1)*sizeof(double *));
+                table[l1][l2][l - abs_val] = malloc((2*l+1)*sizeof(double *));
 
                 for (long m = -l; m<=l; ++m)
                 {
-                    clebsch_gordan_table[l1][l2][l- absolute_value(l1-l2)][m+l] 
-                        = malloc((clebsch_gordan_upper_bound(l1, l2, m) - clebsch_gordan_lower_bound(l1, l2, m) + 1)*sizeof(double));
+                    table[l1][l2][l - abs_val][m+l] 
+                        = malloc((cg_upper_bound(l1, l2, m) - cg_lower_bound(l1, l2, m) + 1)*sizeof(double));
                 }
             }
         }
     }
 
-    return clebsch_gordan_table;
+    return table;
 }
 
-void free_memory_for_clebsch_gordan_coefficients(const size_t bandlimit, cg_table clebsch_gordan_table)
+void destroy_cg_table(const size_t bandlimit, cg_table table)
 {
-    /* Documentation of memory structure is in allocate_memeory_for_clebsch_gordan_coefficients */
+    /* Documentation of memory structure is in allocate_cg_table */
+
+    long min_val; 
+    long abs_val;
 
     for (long l1 = 0; l1<=bandlimit; ++l1)
     {
         for (long l2 = 0; l2<=bandlimit; ++l2)
         {
-            for (long l = absolute_value(l1-l2); l<=minimum(l1+l2, bandlimit); ++l)
+            min_val = l1+l2>=bandlimit ? bandlimit : l1+l2;
+            abs_val = l1>=l2 ? l1-l2 : l2-l1;
+
+            for (long l = abs_val; l<=min_val; ++l)
             {
                 for (long m = -l; m<=l; ++m)
                 {
-                    free(clebsch_gordan_table[l1][l2][l- absolute_value(l1-l2)][m+l]);
+                    free(table[l1][l2][l - abs_val][m+l]);
                 }
 
-                free(clebsch_gordan_table[l1][l2][l- absolute_value(l1-l2)]);
+                free(table[l1][l2][l- abs_val]);
             }
 
-            free(clebsch_gordan_table[l1][l2]);
+            free(table[l1][l2]);
         }
 
-        free(clebsch_gordan_table[l1]);
+        free(table[l1]);
     }
 
-    free(clebsch_gordan_table);
-}
-
-
-bool validate_clebsch_gordan_table_order(const size_t bandlimit, const long l1, const long l2, const long l, const long m, const long m1)
-{
-    /* Documentation of memory structure is in allocate_memeory_for_clebsch_gordan_coefficients */
-    
-    if (l1 > bandlimit || l1 < 0)
-    {
-        return false;
-    }
-
-    if (l2 > bandlimit || l2 < 0)
-    {
-        return false;
-    }
-
-    if (l < absolute_value(l1-l2) || l > minimum(l1+l2, bandlimit))
-    {
-        return false;
-    }
-
-    if (m < -l || m > l)
-    {
-        return false;
-    }
-
-    if (m < clebsch_gordan_lower_bound(l1, l2, m) || m > clebsch_gordan_upper_bound(l1, l2, m))
-    {
-        return false;
-    }
-
-    return true;
+    free(table);
 }
 
 
 double get_cg(const cg_table table, const long l1, const long l2, const long l, const long m, const long m1)
 {
-    /* Documentation of memory structure is in allocate_memeory_for_clebsch_gordan_coefficients */
+    /* Documentation of memory structure is in allocate_cg_table */
     
-    return table[l1][l2][l-absolute_value(l1-l2)][m+l][m1-clebsch_gordan_lower_bound(l1, l2, m)];
+    return table[l1][l2][l-(l1>=l2 ? l1-l2 : l2-l1)][m+l][m1-cg_lower_bound(l1, l2, m)];
 }
 
 
 void set_cg(cg_table clebsch_gordan_table, const long l1, const long l2, const long l, const long m, const long m1, const double coefficient)
 {
-    /* Documentation of memory structure is in allocate_memeory_for_clebsch_gordan_coefficients */
+    /* Documentation of memory structure is in allocate_cg_table */
     
-    clebsch_gordan_table[l1][l2][l-absolute_value(l1-l2)][m+l][m1-clebsch_gordan_lower_bound(l1, l2, m)] 
+    clebsch_gordan_table[l1][l2][l-absolute_value(l1-l2)][m+l][m1-cg_lower_bound(l1, l2, m)] 
         = coefficient;
 }
