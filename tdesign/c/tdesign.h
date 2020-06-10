01e9259ae459eee372d50f133618e71651506594
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-10 21:04:30 +0300

    began working on the header

--- tdesign/c/tdesign.h
+++ tdesign/c/tdesign.h
@@ -21,3 +21,13 @@ typedef
     tdesign_sph;
 
 typedef enum COORD_SYSTEM {EUC, SPH} COORD_SYSTEM;
+
+
+/* Functions */
+char *bandlimit_to_filename(const size_t bandlimit);
+
+long bandlimit_to_tdesign_length(const size_t bandlimit);
+
+double *allocate_tdesign(const size_t bandlimit, COORD_SYSTEM sys);
+
+void deallocate_tdesign(double *tdesign);
