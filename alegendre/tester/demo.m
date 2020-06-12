2296bdd55e0415ce15e27962ceb387adca6ea698
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-12 15:17:42 +0300

    finished with the demo

--- alegendre/tester/demo.m
+++ alegendre/tester/demo.m
@@ -1,12 +1,15 @@
-l = 200; 
-m = -50; 
+l = 20; 
+m = -10; 
 
 
 N = 10^3; 
 epsi = 10^-3; 
 
 x = linspace(-1+epsi, 1-epsi, 2*N); 
+t = tic();
 y = evalAlegendre(l, m, x);
+t = toc(t);
+disp(['Runtime (sec): ', num2str(t)]);
 plot(x, y)
 
 ax = gca;
