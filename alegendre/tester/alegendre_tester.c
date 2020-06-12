da85edd918d9834508d283f694b5be88d0885237
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-12 11:06:26 +0300

    first draft of binding code with barebone tester

--- alegendre/tester/alegendre_tester.c
+++ alegendre/tester/alegendre_tester.c
@@ -1,1 +1,13 @@
 // TODO
+
+#include <stdio.h>
+
+void alegendre_eval_init_wrapper(double *dsize);
+
+int main()
+{
+    double out;
+    printf("Begin init.\n");
+    alegendre_eval_init_wrapper(&out);
+    printf("End init.\n");
+}
