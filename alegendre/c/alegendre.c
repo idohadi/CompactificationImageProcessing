e862c042cd8f66c62611a7b0bb41f03a65298047
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-12 11:39:07 +0300

    finished the main function which evaluates the legendre polynomial

--- alegendre/c/alegendre.c
+++ alegendre/c/alegendre.c
@@ -1,6 +1,10 @@
 // TODO: write docs in doxygen
 
+#include <math.h>
+#include <float.h>
 #include "alegendre.h"
+
+#define PI 3.14159265358979323846
 
 void alegendre_init()
 {
@@ -8,18 +12,54 @@ void alegendre_init()
     alegendre_eval_init_wrapper(&dsize);
 }
 
-double alegendre(const long l, const long m, const double x)
+double alegendre(const long l, const long m, const double t)
 {
+    /* 
+    Calculates the associated Legendre polynomial P_{l}^{m} (cos(t)).
+
+    Must run alegendre_init once before using this function.
+    */
+
     double alpha, alphader, vallogp, vallogq, valp, valq;
-    alegendre_eval_wrapper(l, -m, x, 
+    if (t>PI/2 || t<-PI/2)
+    {
+        return 0.0;
+    }
+
+    if (t>=0)
+    {
+        alegendre_eval_wrapper(l, -m, t, 
                             &alpha, &alphader, 
                             &vallogp, &vallogq, 
                             &valp, &valq);
+    }
+    else
+    {
+        alegendre_eval_wrapper(l, -m, -t, 
+                            &alpha, &alphader, 
+                            &vallogp, &vallogq, 
+                            &valp, &valq);
+    }
+    
+    if (valp<=DBL_EPSILON && valp>=-DBL_EPSILON)
+    {
+        return 0.0;
+    }
+    else
+    {
+        return valp/sqrt(2.0*PI*sin(t));
+    }
+    
     return valp;
 }
 
 long alegendre_root_count(const long l, const long m)
 {
+    /* 
+    TODO: docs
+
+    Must run alegendre_init once before using this function.
+    */
     long nproots, nqroots;
     alegendre_nroots_wrapper(l, -m, &nproots, &nqroots);
     return nproots;
@@ -27,6 +67,11 @@ long alegendre_root_count(const long l, const long m)
 
 double alegendre_root(const long l, const long m, const long root_order)
 {
+    /* 
+    TODO: document and also make sure to understand exactly what it does and that the return value is what I want.
+
+    Must run alegendre_init once before using this function.
+    */
     double x;
     alegendre_proot_wrapper(l, -m, root_order, &x);
     return x;
