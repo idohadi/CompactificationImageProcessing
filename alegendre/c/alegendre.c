51b74c53e2ba195a585f24c776d9a5db3bac1a01
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-12 11:26:31 +0300

    wrote the pure C functions

--- alegendre/c/alegendre.c
+++ alegendre/c/alegendre.c
@@ -2,17 +2,32 @@
 
 #include "alegendre.h"
 
+void alegendre_init()
+{
+    double dsize;
+    alegendre_eval_init_wrapper(&dsize);
+}
+
 double alegendre(const long l, const long m, const double x)
 {
-    // TODO
+    double alpha, alphader, vallogp, vallogq, valp, valq;
+    alegendre_eval_wrapper(l, -m, x, 
+                            &alpha, &alphader, 
+                            &vallogp, &vallogq, 
+                            &valp, &valq);
+    return valp;
 }
 
 long alegendre_root_count(const long l, const long m)
 {
-    // TODO
+    long nproots, nqroots;
+    alegendre_nroots_wrapper(l, -m, &nproots, &nqroots);
+    return nproots;
 }
 
 double alegendre_root(const long l, const long m, const long root_order)
 {
-    // TODO
+    double x;
+    alegendre_proot_wrapper(l, -m, root_order, &x);
+    return x;
 }
