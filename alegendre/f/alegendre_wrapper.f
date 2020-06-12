6840988757cfe921225e705722198689b2384280
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-12 09:26:11 +0300

    wrote the wrapper functions, first draft

--- alegendre/f/alegendre_wrapper.f
+++ alegendre/f/alegendre_wrapper.f
@@ -4,11 +4,15 @@
 !This is a wrapper of some functions from Bremer's code, designed to enable C binding
 !
 
-use extern/ALegendreEval/alegendreeval
+USE extern/ALegendreEval/alegendreeval
+USE ISO_C_BINDING
+
+
 
 
 SUBROUTINE alegendre_eval_init_wrapper (dsize)
-	!TODO
+	IMPLICIT NONE
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: dsize
 	
 	CALL alegendre_eval_init (dsize)
 RETURN
@@ -16,7 +20,16 @@ END SUBROUTINE
 
 
 SUBROUTINE alegendre_eval_wrapper (dnu, dmu, t, alpha, alphader, vallogp, vallogq, valp, valq)
-	!TODO
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: dnu
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: dmu
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: t
+	
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: alpha
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: alphader
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: vallogp
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: vallogq
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: valp
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: valq
 	
 	CALL alegendre_eval (dnu, dmu, t, alpha, alphader, vallogp, vallogq, valp, valq)
 RETURN
@@ -24,7 +37,11 @@ END SUBROUTINE
 
 
 SUBROUTINE alegendre_nroots_wrapper (dnu, dmu, nproots, nqroots)
-	!TODO
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: dnu
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: dmu
+	
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: nproots
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: nqroots
 	
 	CALL alegendre_nroots (dnu, dmu, nproots, nqroots)
 RETURN
@@ -32,7 +49,11 @@ END SUBROUTINE
 
 
 SUBROUTINE alegendre_proot_wrapper (dnu, dmu, j, t)
-	!TODO
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: dnu
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: dmu
+	DOUBLE PRECISION (C_DOUBLE), VALUE 			:: j
+	
+	DOUBLE PRECISION (C_DOUBLE), INTENT(OUT) 	:: t
 	
 	CALL alegendre_proot (dnu, dmu, j, t)
 RETURN
