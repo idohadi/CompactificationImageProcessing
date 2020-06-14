5518d9a91994d5dad2e4d9e1865359e6316da5c4
Ido Hadi <idohadi@mail.tau.ac.il>
2020-06-14 08:05:23 +0300

    wrote a tester for the t-design loading function

---
+++ tdesign/tester/tdesign_tester.c
@@ -0,0 +1,29 @@
+// TODO: write docs in doxygen
+/* 
+USE:
+        tdesign_tester bandlimit
+    where 
+        bandlimit is non-negative integer
+*/
+
+#include <stdint.h>
+#include "tdesign.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc==2)
+    {
+        size_t bandlimit;
+        sscanf(argv[2], "%zu", &bandlimit);
+
+        tdesign_cart td = read_tdesign(bandlimit);
+
+        print_tdesign(&td);
+        
+        return 0;
+    }
+    else
+    {
+        printf("Wrong number of arguments.");
+    }
+}
