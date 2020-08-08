cb13512f525d6b910a3662bc5131f0d5928e3452
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-09 00:41:04 +0300

    wrote a setup file

---
+++ setup.m
@@ -0,0 +1,10 @@
+%% Setup file
+[p1, ~, ~] = fileparts(mfilename('fullpath'));
+
+%% Run external setup files
+run(fullfile(p1, 'extern', 'FastSphericalHarmonicsTransform', 'setup.m'));
+run(fullfile(p1, 'extern', 'SmallRotationToolbox', 'setup.m'));
+
+%% Adds the necessary folders to path
+addpath(p1);
+addpath(genpath(fullfile(p1, 'clebsch_gordan')));
