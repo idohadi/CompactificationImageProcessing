33520c4ad26e1ca0e24fbe57f63c02d459c0d8d1
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-09 15:52:20 +0300

    done

--- alignment/rotateSHCEval.m
+++ alignment/rotateSHCEval.m
@@ -1,20 +1,21 @@
-function rotatedSHC = rotateSHCEval(shc, N, rotation)
+function rotatedSHC = rotateSHCEval(shc, N, rotation, assocLegMat, omega, theta, phi)
 %%
 % Call format
-%   rotatedSHC = rotateSHCEval(shc, N, rotation)
-%   values = fsht(shc, N, assocLegMat, phi, nthreads)
+%   rotatedSHC = rotateSHCEval(shc, N, rotation, assocLegMat, omega, theta, phi)
 % 
 % Calculate the spherical harmonics coefficients of the composition of a
 % spherical function with a rotation.
 % 
 % This function rotated the spherical harmonics coefficients using
 % the following procedure:
-%   * Apply rotation to the meshgrid defined by theta and phi.
-%   * Apply the forward spherical harmonics transform to compute the values
-%     of the spherical function on the rotated grid.
-%   * Calculate the spherical harmonics expansion of the rotated spherical 
-%     function by applying the inverse spherical harmonics transform to the
-%     computed values.
+%   1. Apply rotation to the meshgrid defined by theta and phi.
+%   2. Apply the forward spherical harmonics transform to compute the values
+%      of the spherical function on the rotated grid.
+%      These values are the values of the rotated spherical function on the
+%      original grid.
+%   3. Calculate the spherical harmonics expansion of the rotated spherical 
+%      function by applying the inverse spherical harmonics transform to the
+%      computed values.
 % 
 % Input arguments
 %   shc             double      (2*N)^2 x 1 array, 
@@ -23,7 +24,6 @@ function rotatedSHC = rotateSHCEval(shc, N, rotation)
 %                               2*N-1.
 %   rotation        double      4 x 1 array, a rotation in quaternion
 %                               representation.
-
 %   assocLegMat     double      cell array of matrices such that
 %                               assocLegMat{m+2*N} is a (2*M-1) x (4*N-m)
 %                               array satisfying
@@ -33,7 +33,11 @@ function rotatedSHC = rotateSHCEval(shc, N, rotation)
 %                                       0<=m<=2N-1 and m<=l<=2*N-1,
 %                               where >P_{l}^{|m|} is a scaled, normalized
 %                               associated Legendre function.
-%   phi             double      K x 1 array, grid on [0,2pi].
+%   omega           double      2N x 1 array, Guass-Legendre cubature
+%                               weights.
+%   theta           double      2N x 1 array, arccos of Guass-Legendre 
+%                               cubature points.
+%   phi             double      (4N-1) x 1 array, grid on [0, 2pi].
 % 
 % Output arguments
 %   rotated SHC     double      spherical harmonics coefficients of the
@@ -51,4 +55,16 @@ function rotatedSHC = rotateSHCEval(shc, N, rotation)
 % Year      2020
 % ***********************************************************
 %% Rotate SHC
-% TODO
+% Step 1
+[x, y, z] = sph2cart2(theta, phi, 1);
+rotatedPoints = applyRotation(rotation, [x'; y'; z']);
+[rotatedTheta, rotatedPhi, ~] = cart2sph2(rotatedPoints(1, :), ...
+                            rotatedPoints(2, :), ...
+                            rotatedPoints(3, :));
+
+% Step 2
+assocLegMatRotGrid = assocLegendreMatrices(N, rotatedTheta);
+values = fsht(shc, N, assocLegMatRotGrid, rotatedPhi);
+
+% Step 3
+rotatedSHC = isht(values, N, assocLegMat, omega, phi);
