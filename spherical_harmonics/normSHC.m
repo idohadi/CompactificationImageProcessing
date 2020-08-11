e6603d78b81d4b605629e366bff10666b200d233
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-11 10:42:30 +0300

    removed the insitu option

--- spherical_harmonics/normSHC.m
+++ spherical_harmonics/normSHC.m
@@ -1,9 +1,7 @@
-function nshc = normSHC(shc, bandlimit, insitu)
+function nshc = normSHC(shc, bandlimit)
 %%
 % Call format
 %   nshc = normSHC(shc, bandlimit)
-%   nshc = normSHC(shc, bandlimit, 1)
-%   nshc = normSHC(shc, bandlimit, 0)
 % 
 % Normalize spherical harmonics coefficients array to have a power spectrum
 % of all ones.
@@ -12,13 +10,6 @@ function nshc = normSHC(shc, bandlimit, insitu)
 %   shc             double      (bandlimit+1)^2 x N complex array, 
 %                               spherical harmonics coefficients.
 %   bandlimit       double      positive integer, the bandlimit of shc.
-% 
-% Optional arguments
-%   Name            Default     Definition
-%   insitu          1           Decides whether the normalization is
-%                               performed in situ or not.
-%                               1 - in situ.
-%                               0 - not in situ.
 % 
 % Output arguments
 %   nshc            double      (2*N)^2 x 1 complex array, spherical 
@@ -36,10 +27,5 @@ function nshc = normSHC(shc, bandlimit, insitu)
 % Email     idohadi@mail.tau.ac.il
 % Year      2020
 % ***********************************************************
-%% Input validation
-if nargin<3
-    insitu = 1;
-end
-
 %% Normalizing spherical harmonics coefficients
-nshc = normSHC_mex(shc, real(shc.*conj(shc)), bandlimit, insitu);
+nshc = normSHC_mex(shc, real(shc.*conj(shc)), bandlimit);
--- spherical_harmonics/normSHC_mex.c
+++ spherical_harmonics/normSHC_mex.c
@@ -2,16 +2,13 @@
  * Normalize spherical harmonics coefficients array to have a power spectrum of all ones.
  * 
  * MATLAB call form:
- *      nshc = normSHC_mex(shc, shcAbsSqr, bandlimit, insitu)
+ *      nshc = normSHC_mex(shc, shcAbsSqr, bandlimit)
  *  where
  *      shc         (bandlimit+1)^2 x N complex array such that shc(:, n) are spherical 
  *                  harmoncis coefficients of a function of bandlimit
  *      shcAbsSqr   (bandlimit+1)^2 x N complex array such that 
  *                      shcAbsSqr(j, n) = shc(j, n) * conj(shc(j, n))
  *      bandlimit   scalar, the bandlimit of the function represented by the spherical harmonics cofficients
- *      insitu      scalar, 
- *                      1 - if the computation should be done in situ
- *                      0 - if not
  * 
  * NOTES:
  *  (1) The code performs no input checks.
@@ -27,7 +24,6 @@
 mxComplexDouble *shc;
 double *shcAbsSqr;
 size_t bandlimit;
-int insitu;
 
 mxComplexDouble *nshc;
 
@@ -40,27 +36,18 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     shc = mxGetComplexDoubles(prhs[0]);
     shcAbsSqr = mxGetDoubles(prhs[1]);
     bandlimit = mxGetScalar(prhs[2]);
-    insitu = mxGetScalar(prhs[3]);
     
-    N = mxGetN(prhs[0]);
     M = mxGetM(prhs[0]);
+    N = mxGetN(prhs[0]);
 
     // Create output
-    if (insitu==1)
+    plhs[0] = mxCreateDoubleMatrix(M, N, mxCOMPLEX);
+    nshc = mxGetComplexDoubles(plhs[0]);
+    for (long n = 0; n<N*M; ++n)
     {
-        plhs[0] = prhs[0];
-        nshc = mxGetComplexDoubles(plhs[0]);
+        nshc[n] = shc[n];
     }
-    else
-    {
-        plhs[0] = mxCreateDoubleMatrix(M, N, mxCOMPLEX);
-        nshc = mxGetComplexDoubles(plhs[0]);
-        for (long n = 0; n<mxGetNumberOfElements(plhs[0]); ++n)
-        {
-            nshc[n] = shc[n];
-        }
-    }
-
+    
     // Compute output
     double norm;
     for (size_t n = 0; n<N; ++n)
