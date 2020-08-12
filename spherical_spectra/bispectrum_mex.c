172dcb571b7c009330576642e5c22f81f079d3af
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-12 04:23:47 +0300

    wrote everything but the bispectrum and gradient calculation functions

--- spherical_spectra/bispectrum_mex.c
+++ spherical_spectra/bispectrum_mex.c
@@ -2,7 +2,7 @@
  * Calculate the bispectrum and its gradient.
  * 
  * MATLAB call form:
- *      b = bispectrum_mex(shc, shc_conj, bandlimit, CGs)
+ *      b = bispectrum_mex(shc, bandlimit)
  *      [b, grad_i, grad_j, grad_vals, grad_nnz, grad_rows_no, grad_cols_no] = bispectrum_mex(shc, shc_conj, bandlimit, CGs)
  *  where
  *      shc                           column or row complex array of length 
@@ -47,57 +47,29 @@ typedef double ***** CGTable;
 
 /* Input variables */
 mxComplexDouble *shc;
-mxComplexDouble *shc_conj;
+// mxComplexDouble *shc_conj;
 size_t bandlimit;
 CGTable cgt;
 mxArray *CGs;
 
 /* Output variables */
 double *b;
-double *grad_inds;
+double *grad_i;
+double *grad_j;
 double *grad_vals;
-double grad_nnz;
-double grad_rows_no;
-double grad_cols_no;
+long grad_nnz;
+long grad_rows_no;
+long grad_cols_no;
 
 
 /* Utility variables */
 blt lookup;
 size_t grad_nnz; // Number of non-zero elements in the bispectrum gradient matrix
+bool first_run = true;
+size_t previous_bandlimit;
 
 
 /* utility functions */
-
-void ccsum(mxComplexDouble *arg1, mxComplexDouble *arg2, mxComplexDouble *output)
-{
-  /** 
-   * Perform output = arg1 + arg2 for complex variables.
-   * */
-
-    output->real = arg1->real + arg2->real;
-    output->imag = arg1->imag + arg2->imag;
-}
-
-void ccprod_addinsitu(mxComplexDouble *arg1, mxComplexDouble *arg2, double factor, mxComplexDouble *output)
-{
-    /** 
-   * Perform output += factor*arg1*arg2 for complex variables.
-   * */
-
-    output->real += factor*(arg1->real*arg2->real - arg1->imag*arg2->imag);
-    output->imag += factor*(arg1->real*arg2->imag + arg1->imag*arg2->real);
-}
-
-void rcprod(double *arg1, mxComplexDouble *arg2, mxComplexDouble *output)
-{
-  /**
-   * Perform output = arg1*arg2 for complex arg2 and real arg1.
-   */
-
-    output->real = (*arg1)*arg2->real;
-    output->imag = (*arg1)*arg2->imag;
-}
-
 void build_bisp_lookup_table()
 {
     grad_nnz = 0;
@@ -142,8 +114,9 @@ void build_bisp_lookup_table()
         }
     }
 
-    grad_rows_no = index + 1;
-    grad_cols_no = (bandlimit+1)*(bandlimit+1);
+    grad_nnz *= 2;
+    grad_rows_no = index;
+    grad_cols_no = 2*(bandlimit+1)*(bandlimit+1);
 }
 
 
@@ -215,116 +188,126 @@ void destroy_CGTable()
 
 /* Functions calculating the bispectrum and its gradient */
 
-void bisp(mxComplexDouble * const input, mxComplexDouble * const input_conjugated, double *output)
-{
-  // TODO
-    mxComplexDouble temp;
-
-    for (long l1 = 0; l1<=bandlimit; ++l1)
-    {
-        for (long l2 = 0; l2<=l1; ++l2)
-        {
-            for (long l = l1-l2; l<=bandlimit && l<=l1+l2; ++l)
-            {
-                bispectral_invariant(input, input_conjugated, l1, l2, l, &temp);
-                output[lookup[l1][l2][l - (l1-l2)][REAL_PART]] = temp.real;
-                output[lookup[l1][l2][l - (l1-l2)][IMAG_PART]] = temp.imag;
-            }
-        }
-    }
-}
-
-void bispGrad(  mxComplexDouble * const input, mxComplexDouble * const input_conjugated, double *output)
+void bisp()
 {
     // TODO
-    
-    mxArray *arr = mxCreateDoubleMatrix((bandlimit+1)*(bandlimit+1), 1, mxCOMPLEX);
-    mxComplexDouble *onei = mxGetComplexDoubles(arr);
-    
-    mxComplexDouble temp;
-
-    size_t row_index_rp, row_index_ip;
-    
-    for (long l1 = 0; l1<=bandlimit; ++l1)
-    {
-        for (long l2 = 0; l2<=l1; ++l2)
-        {
-            for (long l = l1-l2; l<=bandlimit && l<=l1+l2; ++l)
-            {
-                row_index_rp = 2*(bandlimit+1)*(bandlimit+1)*lookup[l1][l2][l-(l1-l2)][REAL_PART];
-                row_index_ip = 2*(bandlimit+1)*(bandlimit+1)*lookup[l1][l2][l-(l1-l2)][IMAG_PART];
-
-                // Derivative of b_{l1,l2,l} w.r.t f_{l1,m1_der}
-                for (long m1_der = -l1; m1_der<=l1; ++m1_der)
-                {
-                    // Derivative w.r.t real part
-                    temp.real = 0;
-                    temp.imag = 0;
-                    onei[l1*(l1+1) + m1_der].real = 1;
-                    sum_defining_bisp(onei, shc_conjugated, shc, l1, l2, l, &temp);
-                    output[row_index_rp + 2*(l1*(l1+1) + m1_der)] += temp.real; // Derivative of real part of b_{l1,l2,l}
-                    output[row_index_ip + 2*(l1*(l1+1) + m1_der)] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
-                    onei[l1*(l1+1) + m1_der].real = 0;
-
-                    // Derivative w.r.t imaginary part
-                    temp.real = 0;
-                    temp.imag = 0;
-                    onei[l1*(l1+1) + m1_der].imag = -1;
-                    sum_defining_bisp(onei, shc_conjugated, shc, l1, l2, l, &temp);
-                    output[row_index_rp + 2*(l1*(l1+1) + m1_der)+1] += temp.real; // Derivative of real part of b_{l1,l2,l}
-                    output[row_index_ip + 2*(l1*(l1+1) + m1_der)+1] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
-                    onei[l1*(l1+1) + m1_der].imag = 0;
-                }
-
-
-                // Derivative of b_{l1,l2,l} w.r.t f_{l2,m2_der}
-                for (long m2_der = -l2; m2_der<=l2; ++m2_der)
-                {
-                    // Derivative w.r.t real part
-                    temp.real = 0;
-                    temp.imag = 0;
-                    onei[l2*(l2+1) + m2_der].real = 1;
-                    sum_defining_bisp(shc_conjugated, onei, shc, l1, l2, l, &temp);
-                    output[row_index_rp + 2*(l2*(l2+1) + m2_der)] += temp.real; // Derivative of real part of b_{l1,l2,l}
-                    output[row_index_ip + 2*(l2*(l2+1) + m2_der)] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
-                    onei[l2*(l2+1) + m2_der].real = 0;
-
-                    // Derivative w.r.t imaginary part
-                    temp.real = 0;
-                    temp.imag = 0;
-                    onei[l2*(l2+1) + m2_der].imag = -1;
-                    sum_defining_bisp(shc_conjugated, onei, shc, l1, l2, l, &temp);
-                    output[row_index_rp + 2*(l2*(l2+1) + m2_der)+1] += temp.real; // Derivative of real part of b_{l1,l2,l}
-                    output[row_index_ip + 2*(l2*(l2+1) + m2_der)+1] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
-                    onei[l2*(l2+1) + m2_der].imag = 0;
-                }
-
-
-                // Derivative of b_{l1,l2,l} w.r.t f_{l,m_der}
-                for (long m_der = -l; m_der<=l; ++m_der)
-                {
-                    // Derivative w.r.t real part
-                    temp.real = 0;
-                    temp.imag = 0;
-                    onei[l*(l+1) + m_der].real = 1;
-                    sum_defining_bisp(shc_conjugated, shc_conjugated, onei, l1, l2, l, &temp);
-                    output[row_index_rp + 2*(l*(l+1) + m_der)] += temp.real; // Derivative of real part of b_{l1,l2,l}
-                    output[row_index_ip + 2*(l*(l+1) + m_der)] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
-                    onei[l*(l+1) + m_der].real = 0;
-
-                    // Derivative w.r.t imaginary part
-                    temp.real = 0;
-                    temp.imag = 0;
-                    onei[l*(l+1) + m_der].imag = 1;
-                    sum_defining_bisp(shc_conjugated, shc_conjugated, onei, l1, l2, l, &temp);
-                    output[row_index_rp + 2*(l*(l+1) + m_der)+1] += temp.real; // Derivative of real part of b_{l1,l2,l}
-                    output[row_index_ip + 2*(l*(l+1) + m_der)+1] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
-                    onei[l*(l+1) + m_der].imag = 0;
-                }
-            }
-        }
-    }
 }
+
+void bisp_grad()
+{
+    // TODO
+}
+
+// void bisp(mxComplexDouble * const input, mxComplexDouble * const input_conjugated, double *output)
+// {
+//   // TODO
+//     mxComplexDouble temp;
+
+//     for (long l1 = 0; l1<=bandlimit; ++l1)
+//     {
+//         for (long l2 = 0; l2<=l1; ++l2)
+//         {
+//             for (long l = l1-l2; l<=bandlimit && l<=l1+l2; ++l)
+//             {
+//                 bispectral_invariant(input, input_conjugated, l1, l2, l, &temp);
+//                 output[lookup[l1][l2][l - (l1-l2)][REAL_PART]] = temp.real;
+//                 output[lookup[l1][l2][l - (l1-l2)][IMAG_PART]] = temp.imag;
+//             }
+//         }
+//     }
+// }
+
+// void bispGrad(  mxComplexDouble * const input, mxComplexDouble * const input_conjugated, double *output)
+// {
+//     // TODO
+    
+//     mxArray *arr = mxCreateDoubleMatrix((bandlimit+1)*(bandlimit+1), 1, mxCOMPLEX);
+//     mxComplexDouble *onei = mxGetComplexDoubles(arr);
+    
+//     mxComplexDouble temp;
+
+//     size_t row_index_rp, row_index_ip;
+    
+//     for (long l1 = 0; l1<=bandlimit; ++l1)
+//     {
+//         for (long l2 = 0; l2<=l1; ++l2)
+//         {
+//             for (long l = l1-l2; l<=bandlimit && l<=l1+l2; ++l)
+//             {
+//                 row_index_rp = 2*(bandlimit+1)*(bandlimit+1)*lookup[l1][l2][l-(l1-l2)][REAL_PART];
+//                 row_index_ip = 2*(bandlimit+1)*(bandlimit+1)*lookup[l1][l2][l-(l1-l2)][IMAG_PART];
+
+//                 // Derivative of b_{l1,l2,l} w.r.t f_{l1,m1_der}
+//                 for (long m1_der = -l1; m1_der<=l1; ++m1_der)
+//                 {
+//                     // Derivative w.r.t real part
+//                     temp.real = 0;
+//                     temp.imag = 0;
+//                     onei[l1*(l1+1) + m1_der].real = 1;
+//                     sum_defining_bisp(onei, shc_conjugated, shc, l1, l2, l, &temp);
+//                     output[row_index_rp + 2*(l1*(l1+1) + m1_der)] += temp.real; // Derivative of real part of b_{l1,l2,l}
+//                     output[row_index_ip + 2*(l1*(l1+1) + m1_der)] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
+//                     onei[l1*(l1+1) + m1_der].real = 0;
+
+//                     // Derivative w.r.t imaginary part
+//                     temp.real = 0;
+//                     temp.imag = 0;
+//                     onei[l1*(l1+1) + m1_der].imag = -1;
+//                     sum_defining_bisp(onei, shc_conjugated, shc, l1, l2, l, &temp);
+//                     output[row_index_rp + 2*(l1*(l1+1) + m1_der)+1] += temp.real; // Derivative of real part of b_{l1,l2,l}
+//                     output[row_index_ip + 2*(l1*(l1+1) + m1_der)+1] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
+//                     onei[l1*(l1+1) + m1_der].imag = 0;
+//                 }
+
+
+//                 // Derivative of b_{l1,l2,l} w.r.t f_{l2,m2_der}
+//                 for (long m2_der = -l2; m2_der<=l2; ++m2_der)
+//                 {
+//                     // Derivative w.r.t real part
+//                     temp.real = 0;
+//                     temp.imag = 0;
+//                     onei[l2*(l2+1) + m2_der].real = 1;
+//                     sum_defining_bisp(shc_conjugated, onei, shc, l1, l2, l, &temp);
+//                     output[row_index_rp + 2*(l2*(l2+1) + m2_der)] += temp.real; // Derivative of real part of b_{l1,l2,l}
+//                     output[row_index_ip + 2*(l2*(l2+1) + m2_der)] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
+//                     onei[l2*(l2+1) + m2_der].real = 0;
+
+//                     // Derivative w.r.t imaginary part
+//                     temp.real = 0;
+//                     temp.imag = 0;
+//                     onei[l2*(l2+1) + m2_der].imag = -1;
+//                     sum_defining_bisp(shc_conjugated, onei, shc, l1, l2, l, &temp);
+//                     output[row_index_rp + 2*(l2*(l2+1) + m2_der)+1] += temp.real; // Derivative of real part of b_{l1,l2,l}
+//                     output[row_index_ip + 2*(l2*(l2+1) + m2_der)+1] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
+//                     onei[l2*(l2+1) + m2_der].imag = 0;
+//                 }
+
+
+//                 // Derivative of b_{l1,l2,l} w.r.t f_{l,m_der}
+//                 for (long m_der = -l; m_der<=l; ++m_der)
+//                 {
+//                     // Derivative w.r.t real part
+//                     temp.real = 0;
+//                     temp.imag = 0;
+//                     onei[l*(l+1) + m_der].real = 1;
+//                     sum_defining_bisp(shc_conjugated, shc_conjugated, onei, l1, l2, l, &temp);
+//                     output[row_index_rp + 2*(l*(l+1) + m_der)] += temp.real; // Derivative of real part of b_{l1,l2,l}
+//                     output[row_index_ip + 2*(l*(l+1) + m_der)] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
+//                     onei[l*(l+1) + m_der].real = 0;
+
+//                     // Derivative w.r.t imaginary part
+//                     temp.real = 0;
+//                     temp.imag = 0;
+//                     onei[l*(l+1) + m_der].imag = 1;
+//                     sum_defining_bisp(shc_conjugated, shc_conjugated, onei, l1, l2, l, &temp);
+//                     output[row_index_rp + 2*(l*(l+1) + m_der)+1] += temp.real; // Derivative of real part of b_{l1,l2,l}
+//                     output[row_index_ip + 2*(l*(l+1) + m_der)+1] += temp.imag; // Derivative of imag part of b_{l1,l2,l}
+//                     onei[l*(l+1) + m_der].imag = 0;
+//                 }
+//             }
+//         }
+//     }
+// }
 
 /**
  * The MEX Function
@@ -332,14 +315,64 @@ void bispGrad(  mxComplexDouble * const input, mxComplexDouble * const input_con
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // Obtain input
-    // TODO
+    shc = mxGetComplexDoubles(prhs[0]);
+    bandlimit = mxGetScalar(prhs[1]);
 
     // Handle first run
-    // TODO
+    if (first_run==true)
+    {
+        build_bisp_lookup_table();
+
+        CGs = mexGetVariablePtr("global", "CGs");
+        create_CGTable();
+
+        previous_bandlimit = bandlimit;
+        first_run = false;
+    }
+
+    // Handle change of bandlimit
+    if (bandlimit!=previous_bandlimit)
+    {
+        previous_bandlimit = bandlimit;
+
+        destory_bisp_lookup_table();
+        destroy_CGTable();
+
+        mxArray *inputMxArray[1];
+        inputMxArray[0] = mxCreateDoubleScalar(bandlimit);
+        mexCallMATLAB(0, NULL, 1, inputMxArray, "loadCGTable");
+
+        CGs = mexGetVariablePtr("global", "CGs");
+
+        build_bisp_lookup_table();
+        create_CGTable();
+    }
 
     // Create output
-    // TODO
+    plhs[0] = mxCreateDoubleMatrix(grad_rows_no, 1, mxREAL);
+    b = mxGetDoubles(plhs[0]);
+
+    if (nlhs>1)
+    {
+        plhs[1] = mxCreateDoubleMatrix(grad_nnz, 1, mxREAL);
+        grad_i = mxGetDoubles(plhs[1]);
+
+        plhs[2] = mxCreateDoubleMatrix(grad_nnz, 1, mxREAL);
+        grad_j = mxGetDoubles(plhs[2]);
+
+        plhs[3] = mxCreateDoubleMatrix(grad_nnz, 1, mxREAL);
+        grad_vals = mxGetDoubles(plhs[3]);
+
+        plhs[4] = mxCreateDoubleScalar((double) grad_nnz);
+        plhs[5] = mxCreateDoubleScalar((double) grad_rows_no);
+        plhs[6] = mxCreateDoubleScalar((double) grad_cols_no);
+    }
 
     // Compute output
-    // TODO
+    bisp();
+
+    if (nlhs>1)
+    {
+        grad_bisp();
+    }
 }
