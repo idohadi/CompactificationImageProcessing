618a6e67e1706dc71db8de1a3e4e005bee23e248
Ido Hadi <idohadi@mail.tau.ac.il>
2020-09-05 09:32:41 +0300

    y

---
+++ spherical_spectra/clebsch_gordan_coefficients.h
@@ -0,0 +1,49 @@
+// TODO: make sure it's correct
+
+#include <stdint.h>
+
+/* Typedef */
+typedef double***** clebsch_gordan_table;
+typedef 
+    struct cg_table
+    {
+        clebsch_gordan_table table;
+        size_t bandlimit;
+    }  cg_table;
+
+
+
+/* Function declarations */
+long cg_lower_bound(const long l1, const long l2, const long m);
+
+
+long cg_upper_bound(const long l1, const long l2, const long m);
+
+
+double backward_substitution_central_diagonal(const long l1, const long l2, const long l, const long m1, const long m);
+
+
+double backward_substitution_secondary_diagonal(const long l1, const long l2, const long m1, const long m);
+
+
+void cg_vector(const long l1, const long l2, const long l, const long m, double * restrict cg);
+
+
+void calculate_cg_table(const size_t bandlimit, cg_table *table);
+
+
+cg_table allocate_cg_table(const size_t bandlimit);
+
+
+void destroy_cg_table(cg_table *table);
+
+
+double get_cg(const cg_table *table, const long l1, const long l2, const long l, const long m, const long m1);
+
+
+void set_cg(cg_table *table, const long l1, const long l2, const long l, const long m, const long m1, const double coefficient);
+
+
+void print_cg(cg_table *table);
+
+void print_cg_vector(long l1, long l2, long l, long m);
