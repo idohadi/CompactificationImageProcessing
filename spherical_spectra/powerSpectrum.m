56cbce6da99b48f7817f0e74680ef5477a00cee8
Ido Hadi <idohadi@mail.tau.ac.il>
2020-09-06 13:46:53 +0300

    checked out from the new code base the function computing the power spectrum

---
+++ spherical_spectra/powerSpectrum.m
@@ -0,0 +1,32 @@
+function powSpec = powerSpectrum(shc, bandlimit)
+%%
+% Call format
+%   powSpec = powerSpectrum(shc, bandlimit)
+% 
+% Calculate the power spectrum of the bandlimited spherical function represented by
+% spherical harmonics ceofficients in shc
+% 
+% Input arguments
+%   shc             double      (bandlimit+1)^2 x 1 complex array, 
+%                               spherical harmonics coefficients.
+%   bandlimit       double      positive integer, the bandlimit of shc.
+% 
+% Output arguments
+%   powSpec         double      (bandlimit+1) x 1 array, the power spectrum
+%                               of the spherical function represented by
+%                               shc, such that
+%                                   powSpec(l+1) = l-order spectra of shc.
+% 
+% Notes
+%   (1) This function depends on powerSpectrum_mex.
+%   (2) This function performs no input checks.
+% 
+% Reference
+%   None
+% ***********************************************************
+% Author    Ido Hadi
+% Email     idohadi@mail.tau.ac.il
+% Year      2020
+% ***********************************************************
+%% Calculate power spectrum
+powSpec = powerSpectrum_mex(shc.*conj(shc), bandlimit);
