ab2b24dbc3c317422003144771f3b325e44ff33a
Ido Hadi <idohadi@mail.tau.ac.il>
2020-09-24 01:33:16 +0300

    another attempt to fix the bug

--- clebsch_gordan/clebsch_gordan_mex.c
+++ clebsch_gordan/clebsch_gordan_mex.c
@@ -5,26 +5,26 @@ void create_CGTable(CGTable * const cgt, const mxArray ** CGs, const size_t bl)
 {
     mxArray *temp[3];
 
-    *cgt = malloc((bl+1)*sizeof(double ****));
+    (*cgt) = malloc((bl+1)*sizeof(double ****));
 
     for (long l1 = 0; l1<=bl; ++l1)
     {
         temp[0] = mxGetCell(*CGs, l1);
-        *cgt[l1] = malloc((l1+1)*sizeof(double ***));
+        (*cgt)[l1] = malloc((l1+1)*sizeof(double ***));
 
         for (long l2 = 0; l2<=l1; ++l2)
         {
             temp[1] = mxGetCell(temp[0], l2);
-            *cgt[l1][l2] = malloc(((l1+l2>=bl ? bl : l1+l2) - (l1-l2) + 1)*sizeof(double **));
+            (*cgt)[l1][l2] = malloc(((l1+l2>=bl ? bl : l1+l2) - (l1-l2) + 1)*sizeof(double **));
 
             for (long l = l1-l2; l<=l1+l2 && l<=bl; ++l)
             {
                 temp[2] = mxGetCell(temp[1], l-(l1-l2));
-                *cgt[l1][l2][l-(l1-l2)] = malloc((2*l+1)*sizeof(double *));
+                (*cgt)[l1][l2][l-(l1-l2)] = malloc((2*l+1)*sizeof(double *));
 
                 for (long m = -l; m<=l; ++m)
                 {
-                    *cgt[l1][l2][l-(l1-l2)][m+l] = mxGetDoubles(mxGetCell(temp[2], m+l));
+                    (*cgt)[l1][l2][l-(l1-l2)][m+l] = mxGetDoubles(mxGetCell(temp[2], m+l));
                 }
             }
         }
@@ -39,11 +39,11 @@ void destroy_CGTable(CGTable * const cgt, const size_t bl)
         {
             for (long l = l1-l2; l<=l1+l2 && l<=bl; ++l)
             {
-                free(*cgt[l1][l2][l-(l1-l2)]);
+                free((*cgt)[l1][l2][l-(l1-l2)]);
             }
-            free(*cgt[l1][l2]);
+            free((*cgt)[l1][l2]);
         }
-        free(*cgt[l1]);
+        free((*cgt)[l1]);
     }
     free(*cgt);
 }
