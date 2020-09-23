3aa082713c00f9a3654164f38105fa0d95199f0a
Ido Hadi <idohadi@mail.tau.ac.il>
2020-09-24 00:43:46 +0300

    wrote a mex interface

--- clebsch_gordan/clebsch_gordan_mex.h
+++ clebsch_gordan/clebsch_gordan_mex.h
@@ -1,1 +1,11 @@
 #include "mex.h"
+
+
+typedef double ***** CGTable;
+
+
+void create_CGTable(CGTable * const cgt, const mxArray *CGs, const size_t bl);
+
+void destroy_CGTable(CGTable * const cgt, const size_t bl);
+
+double get_cg(CGTable * const cgt, const long l1, const long l2, const long l, const long m, const long m1);
