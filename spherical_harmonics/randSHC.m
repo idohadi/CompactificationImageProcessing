f0ba76cf31bccb4219cfc51d98e8abeb48257f0e
Ido Hadi <idohadi@mail.tau.ac.il>
2020-09-23 21:54:15 +0300

    edited docs

--- spherical_harmonics/randSHC.m
+++ spherical_harmonics/randSHC.m
@@ -5,21 +5,22 @@ function shc = randSHC(bandlimit, N)
 %   shc = randSHC(bandlimit, N)
 % 
 % Uniformly sample the spherical harmonics coefficients having power
-% spectrum of all ones and representing a function with bandlimit
-% bandlimit.
+% spectrum of all ones and representing a complex-valued function with 
+% bandlimit bandlimit.
 % 
 % Input arguments
 %   bandlimit       double      positive integer, the bandlimit of shc.
 % 
 % Output arguments
-%   nshc            double      (bandlimit+1)^2 x N complex array, spherical 
+%   shc             double      (bandlimit+1)^2 x N complex array, spherical 
 %                               harmonics coefficients, normalized such
-%                               that it has a power spectrm of all ones.
+%                               that it has a power spectrm of all ones, 
+%                               representing a complex-valued, bandlimited
+%                               spherical function..
 % 
 % Optional arguments
 %   Name            Default         Definition
-%   N               1               Number of spherical harmonics
-%                                   coefficients to sample.
+%   N               1               Number of spherical functions to sample.
 % 
 % Notes
 %   (1) This function performs no input checks.
@@ -32,6 +33,7 @@ function shc = randSHC(bandlimit, N)
 % Email     idohadi@mail.tau.ac.il
 % Year      2020
 % ***********************************************************
+
 %% Input validation
 if nargin<2
     N = 1;
