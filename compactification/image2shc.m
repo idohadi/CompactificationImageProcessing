83566e4fc519dce31725ea65d209536d620f7b26
Ido Hadi <idohadi@mail.tau.ac.il>
2020-09-23 04:05:16 +0300

    updated the codebase and the docs

--- compactification/image2shc.m
+++ compactification/image2shc.m
@@ -1,43 +1,32 @@
-function shc = image2shc(im, evalPoints, N, assocLegMat, omega, phi)
-% TODO: update docs, remove dependency on fast fourier transform, change
-% name to image2shc.
-
+function shc = image2shc(im, bandlimit, tDesign, interval, scalingParam)
 %%
 % Call format
-%   shc = im2shc(im, evalPoints, N, assocLegMat, omega, phi)
+%   shc = image2shc(im, bandlimit, tDesign, interval, scalingParam)
 % 
-% Interpolates the image im to certain evaluation points and use them to
+% Interpolates the image im to a select t-design points and use them to
 % estimate the spherical harmonics coefficients of the projection of the
-% image onto the sphere.
+% image onto the sphere. 
+% 
+% The image is assumed to be the values of a function on an equidistant 
+% grid in the cube interval^2.
 % 
 % Input arguments
-%   im              double      n x m array, an image.
-%   evalPoints      double      k x 2 array, points in R^2.
-%   N               double      positive integer, the bandlimit of shc is
-%                               2*N-1.
-%   assocLegMat     double      cell array of matrices such that
-%                               assocLegMat{m+2*N} is a (2*M-1) x (4*N-m)
-%                               array satisfying
-%                                   assocLegMat{m+2*N}(i, l-m+1) 
-%                                       = >P_{l}^{|m|} (cos(theta(i)))
-%                                   for 
-%                                       0<=m<=2N-1 and m<=l<=2*N-1,
-%                               where >P_{l}^{|m|} is a scaled, normalized
-%                               associated Legendre function.
-%   omega           double      2N x 1 array, Guass-Legendre cubature
-%                               weights.
-%   theta           double      2N x 1 array, arccos of Guass-Legendre 
-%                               cubature points.
-%   phi             double      (4N-1) x 1 array, grid on [0, 2pi].
-%   
+%   im              double      n x n array, an image
+%   bandlimit       double      positive integer, the bandlimit of shc
+%   tDesign         double      N x 3 array, a spherical design (a 
+%                               t-design) in Cartesian coordiantes.
+%   interval        double      1 x 2 array, interval(1) is the lower bound
+%                               of the interval and interval(2).
+%   scalingParam    double      positive number, scaling parameter for 
+%                               the projection.
+% 
 % Output arguments
-%   shc             double      (2N)^2 x 1 array, complex vector of
-%                               spherical harmonics coefficients ordered 
+%   shc             double      (bandlimit+1)^2 x 1 array, complex vector 
+%                               of spherical harmonics coefficients ordered 
 %                               lexicographically.
 % 
 % Notes
-%   This function depends on the repository
-%   FastSphericalHarmonicsTransform.
+%   This function performs no input checks.
 % 
 % Reference
 %   None
@@ -46,17 +35,34 @@ function shc = image2shc(im, evalPoints, N, assocLegMat, omega, phi)
 % Email     idohadi@mail.tau.ac.il
 % Year      2020
 % ***********************************************************
+
 %% Compute the sphericah harmonics coefficients
-% Compute grid on which such that im(k) is the value given in (X(k), Y(k))
-X = linspace(-0.5, 0.5, size(im, 2));
-Y = linspace(-0.5, 0.5, size(im, 1));
+% t-design in spherical coordinates
+[tDesignTheta, tDesignPhi, ~] ...
+    = cart2sph2(tDesign(:, 1), tDesign(:, 2), tDesign(:, 3));
+[R2phi, R2rho] = KondorBackProj(tDesignTheta, tDesignPhi, scalingParam);
+[R2x, R2y] = pol2cart2(R2phi, R2rho);
+
+% Isolating t-design points falling within interval^2
+tDesignInCube = R2x>=interval(1) && R2x<=interval(2) ...
+    && R2y>=interval(1) && R2y<=interval(2);
+R2x = R2x(tDesignInCube);
+R2y = R2y(tDesignInCube);
+
+% Create a grid in the cube interval^2
+X = linspace(interval{:}, size(im, 1));
+Y = linspace(interval{:}, size(im, 1));
 [X, Y] = meshgrid(X, Y);
 
-% Interpolate the given values to evalPoints
-interpVals = interp2(X, Y, im, ...
-                    evalPoints(:, 1), evalPoints(:, 2), ...
-                    'cubic', 0);
-interpVals = complex(reshape(interpVals, [2*N, 4*N-1]));
+% Value for extrapulation outside interval^2
+extval = 0;
+
+% Interpolate the image to values of the t-design that are back-projected
+% into the cube interval^2
+vals = interp2(X, Y, im, R2x, R2y, 'cubic', extval);
+
+% Compute the spherical harmonics matrix
+sh = sphericalHarmonics(theta, phi, bandlimit);
 
 % Estimate the spherical harmonics coefficients
-shc = isht(interpVals, N, assocLegMat, omega, phi);
+shc = (4*pi/size(tDesign, 1)) * (conj(sh) * vals);
