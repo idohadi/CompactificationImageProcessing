14355753663e20519020938cebf73187862aeacc
Ido Hadi <idohadi@mail.tau.ac.il>
2020-09-24 01:16:47 +0300

    updated docs

--- estimation/buildV.m
+++ estimation/buildV.m
@@ -5,17 +5,18 @@ function V = buildV(bandlimit, U)
 % 
 % Compute the vector V for which
 %           1    N  (                               )
-%   psEst = - * sum ( powerSpectrum(n-th sample SHC) - V )
+%   psEst = - * sum ( powerSpectrum(n-th sample SHC) - sigma^2 * V )
 %           N   n=1 (                               )
 % is an unbiased estimator.
 % 
 % Input arguments
-%   U           double      
 %   bandlimit   double      positive integer, the bandlimit of the sampled
 %                           SHCs.
+%   U           double      matrix, the output of buildU. See description
+%                           there.
 % 
 % Output arguments
-%   P               double      a matrix represneting the linear opeartor
+%   V               double      (bandlimit+1) x 1 array, the vector
 %                               described above.
 % 
 % Notes
