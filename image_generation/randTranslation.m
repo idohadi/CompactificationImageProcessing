bdc1dc1768a95b42e233394e593aaf91fbcefe0e
Ido Hadi <idohadi@mail.tau.ac.il>
2020-10-20 01:46:20 +0300

    edited to generate a list of translations

--- image_generation/randTranslation.m
+++ image_generation/randTranslation.m
@@ -1,40 +1,42 @@
 function [translation, translationAngle, translationSize] ...
-    = randTranslation(maxPixels, lambda)
+    = randTranslation(sampleSize, maxPixels, lambda)
 %%
 % Call format
 %   randTranslation()
-%   randTranslation(maxPixels)
-%   randTranslation(maxPixels, lambda)
+%   randTranslation(sampleSize, maxPixels)
+%   randTranslation(sampleSize, maxPixels, lambda)
 %   translation = randTranslation(__)
 %   [translation, translationAngle] = randTranslation(__)
 %   [translation, translationAngle, translationSize] = randTranslation(__)
 % 
-% Compute a random translation. The translation is later used to transform
-% translate an image with MATLAB's imtranslate function.
+% Compute random translations.
 % 
-% The translation is produced using the following procedure:
+% A translation is produced using the following procedure:
 %   1. Uniformly sample a translate angle from [0, 2pi].
 %   2. Samplea  translation size by repeatedly sampling the distribution
 %       Exp[lambda] until a value below maxPixels is obtained.
 %   3. Compute the translation by 
-%       translation = translationSize * [cos(translateionAngle), sin(translateionAngle)]
+%       translation = translationSize * [cos(translateionAngle); sin(translateionAngle)]
 % 
 % Input arguments
+%   sampleSize          double      positive integer, number of
+%                                   translations to sample.
 %   maxPixels           double      positive scalar, the maximal number of 
 %                                   pixels.
 %   lambda              double      positive scalar, defining parameter of
 %                                   the exponential distribution.
 % Defaults
+%   sampleSize          1
 %   maxPixels           5
 %   lambda              1.5
 % 
 % Output arguments
-%   translation         double      1 x 2 array, described above.
+%   translation         double      2 x sampleSize array, described above.
 %   translationAngle    double      scalar, a number in [0,2pi].
 %   translationSize     double      scalar, described above.
 % 
 % Notes
-%   This function performs no input checks.
+%   None
 % 
 % Reference
 %   None
@@ -45,20 +47,28 @@ function [translation, translationAngle, translationSize] ...
 % ***********************************************************
 
 %% Input handling
-narginchk(0, 2);
+narginchk(0, 3);
 
 if nargin==0
+    sampleSize = 1;
     maxPixels = 5;
     lambda = 1.5;
 elseif nargin==1
+    maxPixels = 5;
+    lambda = 1.5;
+elseif nargin==2
     lambda = 1.5;
 end
 
 %% Generate a random translation
-translationAngle = 2*pi*rand();
-translationSize = Inf;
-while translationSize>maxPixels
-    translationSize = exprnd(lambda);
+translationAngle = 2*pi*rand(1, sampleSize);
+
+translationSize = exprnd(lambda);
+indicator = translationSize>maxPixels;
+while any(indicator)
+    translationSize(indicator) = exprnd(lambda);
+    indicator = translationSize>maxPixels;
 end
+
 translation ...
-    = translationSize*[cos(translationAngle), sin(translationAngle)];
+    = translationSize.*[cos(translationAngle); sin(translationAngle)];
