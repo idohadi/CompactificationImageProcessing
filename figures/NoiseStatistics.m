28e4727c1b58afb73d62450b918c6a63b6092699
Ido Hadi <idohadi@mail.tau.ac.il>
2022-01-16 22:32:12 +0200

    final version

--- figures/NoiseStatistics.m
+++ figures/NoiseStatistics.m
@@ -36,72 +36,60 @@ save(fn, 'sigma', 'imageSize', 'bandlimit', 'interval', 'scalingParam');
 printBegEndMsg('Setup parameters', false);
 
 %% Run test
-image = zeros(imageSize, imageSize, trialNo, length(sigma));
-imagePowSpec = zeros(imageSize, imageSize, trialNo, length(sigma));
-shc = zeros((bandlimit+1)^2, trialNo, length(sigma));
-shcPowSpec = zeros(bandlimit+1, trialNo, length(sigma));
+image = zeros(imageSize, imageSize, trialNo);
+imagePowSpec = zeros(imageSize, imageSize, trialNo);
+shc = zeros((bandlimit+1)^2, trialNo);
+shcPowSpec = zeros(bandlimit+1, trialNo);
 
-for s=1:length(sigma)
-    printBegEndMsg(num2str([s, length(sigma), sigma(s)^2], ...
-        'Variance %d of %d (Sigma^2 = %.3f)'), true);
+% Calculate the images
+printBegEndMsg('Generate white noise images', true);
+image(:, :, :) = sigma^2 * randn(imageSize, imageSize, trialNo);
+printBegEndMsg('Generate white noise images', false);
 
-    % Calculate the images
-    printBegEndMsg('Generate white noise images', true);
-    image(:, :, :, s) = sigma(s)^2 * randn(imageSize, imageSize, trialNo);
-    printBegEndMsg('Generate white noise images', false);
-    
-    % Calculate power spectrum of images
-    printBegEndMsg('Calculate power spectrum of images', true);
-    imagePowSpec = fft2d(image, [1, 2]);
-    imagePowSpec = abs(imagePowSpec).^2;
-    printBegEndMsg('Calculate power spectrum of images', false);
-    
-    % Project the images onto the sphere and compute their power spectrum
-    printBegEndMsg('Project images to sphere and compute power spectrum', true);
-    parfor J=1:size(image, 3)
-        shc(:, J, s) = image2shc(image(:, :, J, s), bandlimit, tDesign, ...
-            interval, scalingParam, sh);
-        shcPowSpec(:, J, s) = powerSpectrum(shc(:, J, s), bandlimit);
-    end
-    printBegEndMsg('Project images to sphere and compute power spectrum', false);
+% Calculate power spectrum of images
+printBegEndMsg('Calculate power spectrum of images', true);
+imagePowSpec = fft2d(image, [1, 2]);
+imagePowSpec = abs(imagePowSpec).^2;
+printBegEndMsg('Calculate power spectrum of images', false);
 
-    printBegEndMsg(num2str([s, length(sigma), sigma(s)^2], ...
-        'Variance %d of %d (Sigma^2 = %.3f)'), false);
+% Project the images onto the sphere and compute their power spectrum
+printBegEndMsg('Project images to sphere and compute power spectrum', true);
+parfor J=1:size(image, 3)
+    shc = image2shc(image(:, :, J), bandlimit, tDesign, ...
+        interval, scalingParam, sh);
+    shcPowSpec(:, J) = powerSpectrum(shc, bandlimit);
 end
-
-% Save result
-%save(fn, 'image', 'imagePowSpec', 'shc', 'shcPowSpec', '-append');
+printBegEndMsg('Project images to sphere and compute power spectrum', false);
 
 %% Generate the mean power spectrum
 printBegEndMsg('Calculating the mean power spectrums', true);
-shcAbsMean = squeeze(mean(abs(shc).^2, 2));
-shcPowSpecMean = squeeze(mean(shcPowSpec, 2));
+shcPowSpecMean = squeeze(mean(shcPowSpec, 2))./(2*(0:bandlimit)'+1);
 imagePowSpecMean = squeeze(mean(imagePowSpec, 3));
 
 % Save result
-save(fn, 'shcAbsMean', 'shcPowSpecMean', 'imagePowSpecMean', '-append');
+save(fn, 'shcPowSpecMean', 'imagePowSpecMean', '-append');
 
 printBegEndMsg('Calculating the mean power spectrums', false);
 
 %% Produce figure
 fig = figure;
-t = tiledlayout(1, 3, ...
+t = tiledlayout(1, 2, ...
     'TileSpacing', 'compact', 'Padding', 'compact');
-
-shcPowSpecMeanNormalized = shcPowSpecMean./(2*(0:bandlimit)'+1);
-shcNorms = zeros(size(shcAbsMean, 1), 1);
-for l=0:bandlimit
-    for m=-l:l
-        shcNorms(l^2 + l + m +1) = l;
-    end
-end
-shcAbsMeanNormalized = shcAbsMean./shcNorms;
-rescell = {shcAbsMean, shcPowSpecMeanNormalized, imagePowSpecMean};
-caxislims = [minCell(rescell), maxCell(rescell)];
+title(t, 'Power Spectrum of White Noise');
 
 nexttile;
+imagesc(imagePowSpecMean);
+colormap('hot');
+colorbar;
 
+title('Image');
 
+nexttile;
+stem(shcPowSpecMean);
+
+title('Projection');
+xlabel('Frequency');
+ylabel('Power Spectrum');
 
 savefig(fig, [fnNOEXT, '.fig']);
 
@@ -112,21 +100,5 @@ diary off;
 function Y = fft2d(X, dims)
 for J=dims
     Y = fft(X, [], J)/size(X, J);
-end
-end
-
-function maxX = maxCell(C)
-maxX = 0;
-for J=1:numel(C)
-    tmp2 = max(C{J}, [], 'all');
-    maxX = max(maxX, tmp2);
-end
-end
-
-function minX = minCell(C)
-minX = 0;
-for J=1:numel(C)
-    tmp2 = min(C{J}, [], 'all');
-    minX = min(minX, tmp2);
 end
 end
