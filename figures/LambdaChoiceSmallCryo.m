cfe32d018af76cc2b0af64130a071fcbb710e285
Ido Hadi <idohadi@mail.tau.ac.il>
2022-01-17 15:37:51 +0200

    renamed files

--- figures/LambdaChoiceSmallCryo.m
+++ figures/LambdaChoiceSmallCryo.m
@@ -11,7 +11,7 @@ rng(0, 'twister');
 %% File setup
 dt = datetime;
 dt = datestr(dt, 'yyyy-mm-dd-HHMM');
-fnNOEXT = ['LambdaChoice-', dt]; 
+fnNOEXT = ['LambdaChoiceSmallCryo-', dt]; 
 diary([fnNOEXT, '.log']); % Log file
 fn = [fnNOEXT, '.mat']; % Output file
 
@@ -22,7 +22,7 @@ lambda = 1:0.1:5;
 bandlimit = 2:2:70;
 imageNo = 50;
 
-imageSize = 101;
+imageSize = 43;
 
 interval = cos(pi/4)*[-1, 1];
 scalingParam = 1;
@@ -65,9 +65,7 @@ printBegEndMsg('Generate ground truth images', false);
 %% Run test
 printBegEndMsg('Running test', true);
 
-upsBackProjErr = zeros(length(lambda), length(bandlimit), imageNo);
 cryoBackProjErr = zeros(length(lambda), length(bandlimit), imageNo);
-upsBackProjRelErr = zeros(length(lambda), length(bandlimit), imageNo);
 cryoBackProjRelErr = zeros(length(lambda), length(bandlimit), imageNo);
 
 for b=1:length(bandlimit)
@@ -87,13 +85,6 @@ for b=1:length(bandlimit)
         [~, sh2] = shc2image(shc, bl, imageSize, interval, scalingParam);
         
         parfor t=1:imageNo
-            % Upsampling
-            upsSHC = image2shc(imUps(:, :, t), bl, ...
-                tDesign, interval, scalingParam, sh);
-            upsIm = shc2image(upsSHC, bl, imageSize, interval, scalingParam, sh2);
-            upsBackProjErr(l, b, t) = norm(upsIm - imUps(:, :, t), 'fro');
-            upsBackProjRelErr(l, b, t) = upsBackProjErr(l, b, t)/norm(imUps(:, :, t), 'fro');
-            
             % Cryo
             cryoSHC = image2shc(imCryo(:, :, t), bl, ...
                 tDesign, interval, scalingParam, sh);
@@ -111,8 +102,8 @@ for b=1:length(bandlimit)
 end
 
 % Save result
-save(fn, 'upsBackProjErr', 'cryoBackProjErr', ...
-    'upsBackProjRelErr', 'cryoBackProjRelErr', ...
+save(fn, 'cryoBackProjErr', ...
+    'cryoBackProjRelErr', ...
     '-append');
 
 printBegEndMsg('Running test', false);
@@ -121,35 +112,27 @@ printBegEndMsg('Running test', false);
 %% Generate the mean for every image
 printBegEndMsg('Calculating the mean over all images', true);
 
-upsBackProjErrMean = squeeze(mean(upsBackProjErr, 3));
 cryoBackProjErrMean = squeeze(mean(cryoBackProjErr, 3));
-upsBackProjRelErrMean = squeeze(mean(upsBackProjRelErr, 3));
 cryoBackProjRelErrMean = squeeze(mean(cryoBackProjRelErr, 3));
 
 % Save result
-save(fn, 'upsBackProjErrMean', 'cryoBackProjErrMean', ...
-    'upsBackProjRelErrMean', 'cryoBackProjRelErrMean', ...
+save(fn, 'cryoBackProjErrMean', ...
+    'cryoBackProjRelErrMean', ...
     '-append');
 
 printBegEndMsg('Calculating the mean over all images', false);
 
 %% Produce figure
 fig = figure;
-t = tiledlayout(1, 2, ...
+t = tiledlayout(1, 1, ...
     'TileSpacing', 'compact', 'Padding', 'compact');
 title(t, 'Relationship of Scaling Parameter and Bandlimit');
 
-caxismin = min([min(cryoBackProjRelErrMean), ...
-    min(upsBackProjRelErrMean)]);
-caxismax = max([max(cryoBackProjRelErrMean), ...
-    max(upsBackProjRelErrMean)]);
-caxislims = [caxismin, caxismax];
 
 nexttile;
 imagesc(cryoBackProjRelErrMean);
 colormap('hot');
 colorbar;
-caxis(caxislims);
 
 xt = 5:5:length(bandlimit);
 xticks(xt);
@@ -165,27 +148,6 @@ ylabel('Scaling parameter');
 
 set(gca,'ColorScale','log')
 
-
-nexttile;
-imagesc(upsBackProjRelErrMean);
-colormap('hot');
-colorbar;
-caxis(caxislims);
-
-xt = 5:5:length(bandlimit);
-xticks(xt);
-xticklabels(bandlimit(xt));
-
-yt = 1:5:length(lambda);
-yticks(yt);
-yticklabels(lambda(yt));
-
-title('Random images');
-xlabel('Bandlimit');
-ylabel('Scaling parameter');
-
-set(gca,'ColorScale','log')
-
 savefig(fig, [fnNOEXT, '.fig']);
 
 %% Shut down the diary
@@ -196,12 +158,5 @@ function im = imagebyUps(tn, imSz)
 im = zeros(imSz, imSz, tn);
 for J=1:tn
     im(:, :, J) = imageByUpsampling(14, 16, imSz, round(0.2*imSz));
-end
-end
-
-
-function Y = fft2d(X, dims)
-for J=dims
-    Y = fft(X, [], J)/size(X, J);
 end
 end
