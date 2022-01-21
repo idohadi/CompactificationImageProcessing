7501b71fe3e204b9a39db8ceccac073afa2f782d
Ido Hadi <idohadi@mail.tau.ac.il>
2022-01-21 14:42:13 +0200

    thm. (2.11) test

---
+++ figures/OrbitApproximationBound.m
@@ -0,0 +1,102 @@
+%% Docs
+% ***********************************************************
+% Author    Ido Hadi
+% Email     idohadi@mail.tau.ac.il
+% Year      2022
+% ***********************************************************
+
+%% Pool setting
+poolSize = 48;
+try
+    parpool(poolSize);
+catch
+    
+end
+
+%% RNG seed
+rng(0, 'twister');
+
+%% File setup
+dt = datetime;
+dt = datestr(dt, 'yyyy-mm-dd-HHMM');
+fnNOEXT = ['ClassificationCryo-', dt]; 
+diary([fnNOEXT, '.log']); % Log file
+fn = [fnNOEXT, '.mat']; % Output file
+
+%% Setup parameters
+printBegEndMsg('Setup parameters', true);
+
+imageNo = 1;
+imageSize = 101;
+
+bandlimit = 50;
+tDesign = loadtd(2*bandlimit);
+interval = cos(pi/4)*[-1, 1];
+scalingParam = 1:0.2:3;
+
+save(fn, 'imageNo', 'imageSize', 'bandlimit', 'interval', 'scalingParam');
+
+printBegEndMsg('Setup parameters', false);
+
+%% Generate images
+printBegEndMsg('Dataset generation', true);
+
+% Image by cryo-EM simulation
+printBegEndMsg('Generate representatives', true);
+root = aspire_root();
+file_name = fullfile(root, 'projections', 'simulation', 'maps', 'cleanrib.mat');
+f = load(file_name);
+vol_true = cryo_downsample(f.volref, imageSize*ones(1, 3));
+
+rotation = rand_rots(imageNo);
+im = cryo_project(vol_true, rotation, imageSize, 'double');
+
+save(fn, 'rotation', 'im', '-append');
+
+printBegEndMsg('Dataset generation', false);
+
+%% Rotation and translation
+printBegEndMsg('Generate rotation and translation', true);
+
+rotation = 2*pi*rand();
+transRot = 2*pi*rand();
+maxTrans = 10;
+translation = maxTrans*rand()*[cos(transRot), sin(transRot)];
+
+save(fn, 'rotation', 'transRot', 'maxTrans', 'translation', '-append');
+
+printBegEndMsg('Generate rotation and translation', false);
+
+
+%% Run test
+printBegEndMsg('Run test', true);
+
+LHS = zeros(length(scalingParam), 1);
+RHS = zeros(length(scalingParam), 1);
+
+for J=1:length(scalingParam)
+    shc = image2shc(im, bandlimit, tDesign, interval, scalingParam(J));
+    P = contractionMap(translation, rotation, scalingParam(J));
+    
+    rotatedSHC = rotateSHC(shc, bandlimit, P, tDesign);
+    
+    imTmp = imrotate(im, 360*rotation/(2*pi), 'bicubic', 'crop');
+    imTmp = imtranslate(imTmp, translation, 'cubic', 'OutputView', 'same');
+    
+    rotatedImSHC = image2shc(imTmp, bandlimit, tDesign, interval, ...
+        scalingParam(J));
+    
+    LHS(J) = log(norm(rotatedImSHC - rotatedSHC, 2));
+    RHS(J) = logbound(translation, 1, scalingParam(J));
+end
+
+
+printBegEndMsg('Run test', false);
+
+%% Utility function
+function out = logbound(b, lambda, lambdatilde)
+out = 0.5*log(1 - cos(lambda*pi/lambdatilde));
+out = out + log(4*pi);
+out = out + norm(b) + lambda*pi;
+out = out - 2*log(lambdatilde);
+end
