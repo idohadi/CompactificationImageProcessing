f09805140d7770faae72eb3474f22f8d23a938b3
Ido Hadi <idohadi@mail.tau.ac.il>
2022-01-21 15:27:02 +0200

    now matches reviewer's request more closely

--- figures/OrbitApproximationBound.m
+++ figures/OrbitApproximationBound.m
@@ -24,9 +24,12 @@ imageSize = 101;
 bandlimit = 50;
 tDesign = loadtd(2*bandlimit);
 interval = cos(pi/4)*[-1, 1];
-scalingParam = 1:0.2:3;
+scalingParam = 1:0.1:5;
 
-save(fn, 'imageNo', 'imageSize', 'bandlimit', 'interval', 'scalingParam');
+sampleSize = 100;
+
+save(fn, 'imageNo', 'imageSize', 'bandlimit', 'interval', ...
+    'scalingParam', 'sampleSize');
 
 printBegEndMsg('Setup parameters', false);
 
@@ -50,10 +53,10 @@ printBegEndMsg('Dataset generation', false);
 %% Rotation and translation
 printBegEndMsg('Generate rotation and translation', true);
 
-rotation = 2*pi*rand();
+rotation = 2*pi*rand(sampleSize, 1);
 transRot = 2*pi*rand();
 maxTrans = 10;
-translation = maxTrans*rand()*[cos(transRot), sin(transRot)];
+translation = maxTrans*rand(sampleSize, 1)*[cos(transRot), sin(transRot)];
 
 save(fn, 'rotation', 'transRot', 'maxTrans', 'translation', '-append');
 
@@ -63,27 +66,28 @@ printBegEndMsg('Generate rotation and translation', false);
 %% Run test
 printBegEndMsg('Run test', true);
 
-LHS = zeros(length(scalingParam), 1);
-RHS = zeros(length(scalingParam), 1);
+LHS = zeros(length(scalingParam), sampleSize);
+RHS = zeros(length(scalingParam), sampleSize);
 
-for J=1:length(scalingParam)
-    shc = image2shc(im, bandlimit, tDesign, interval, scalingParam(J));
-    P = contractionMap(translation, rotation, scalingParam(J));
-    
-    rotatedSHC = rotateSHC(shc, bandlimit, P.', tDesign);
-    
-    R = [cos(rotation), sin(rotation); ...
-        -sin(rotation), cos(rotation)];
-    bb = - (R.' * translation.').';
-    rr = -360*rotation/(2*pi);
-    imTmp = imrotate(im, rr, 'bicubic', 'crop');
-    imTmp = imtranslate(imTmp, bb, 'cubic', 'OutputView', 'same');
-    
-    rotatedImSHC = image2shc(imTmp, bandlimit, tDesign, interval, ...
-        scalingParam(J));
-    
-    LHS(J) = log(norm(rotatedImSHC - rotatedSHC, 2));
-    RHS(J) = logbound(translation, 1, scalingParam(J));
+for n=1:sampleSize
+    for J=1:length(scalingParam)
+        shc = image2shc(im, bandlimit, tDesign, interval, scalingParam(J));
+        P = contractionMap(translation(n), rotation(n), scalingParam(J));
+
+        rotatedSHC = rotateSHC(shc, bandlimit, P.', tDesign);
+
+        R = [cos(rotation(n)), sin(rotation(n)); ...
+            -sin(rotation(n)), cos(rotation(n))];
+        bb = - (R.' * translation(n).').';
+        rr = -360*rotation(n)/(2*pi);
+        imTmp = imrotate(im, rr, 'bicubic', 'crop');
+        imTmp = imtranslate(imTmp, bb, 'cubic', 'OutputView', 'same');
+
+        rotatedImSHC = image2shc(imTmp, bandlimit, tDesign, interval, ...
+            scalingParam(J));
+
+        LHS(J, n) = norm(rotatedImSHC - rotatedSHC, 2);
+    end
 end
 
 save(fn, 'LHS', 'RHS', '-append');
@@ -94,11 +98,23 @@ printBegEndMsg('Run test', false);
 %% Plot the results
 fig = figure;
 
-scatter(LHS, RHS);
+hold;
 
-xlabel('log 2-norm approximation');
-ylabel('log bound');
+plot(scalingParam, mean(LHS, 2), 'MarkerFaceColor', 'blue');
+
+polX = [scalingParam, flip(scalingParam)];
+prctl95 = [prctile(LHS, 97.5, 2), ...
+    flip(prctile(LHS, 2.5, 2))];
+c = [17, 17, 17]/255;
+fill(polX(:), prctl95(:), c, 'FaceAlpha', 0.3, 'LineStyle', 'none');
+
+hold off;
+
+xlabel('$\widetilde{\lambda}$', 'Interpreter', 'latex');
+ylabel('2-norm approx.');
 title('Eq. (2.11) test');
+
+set(gca, 'yscale', 'log');
 
 savefig(fig, [fnNOEXT, '.fig']);
 
